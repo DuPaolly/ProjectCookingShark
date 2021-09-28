91dd33ae64c5f995d4105d53b31f2953ca58617e
Eduarda Paolly <paollyk13@outlook.com>
2021-09-28 17:55:48 -0300

    Pedido.scrip Updated
    The scrip receive the heritage from the Sabores.script. Has been added the variables to choose the tasty, and the prohibited ingredient and all the camps has been serialized

--- ProjectCookingShark/Assets/Script/Pedido.cs
+++ ProjectCookingShark/Assets/Script/Pedido.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pedido : MonoBehaviour
+public class Pedido : Sabores
 {
-    
+    [SerializeField] Sabores.SaboresExistentes saborPedido01;
+    public Sabores.SaboresExistentes SaborPedido01 => saborPedido01;
+
+    [SerializeField] Sabores.SaboresExistentes saborPedido02;
+    public Sabores.SaboresExistentes SaborPedido02 => saborPedido02;
+
+    [SerializeField] Ingrediente ingredienteProibidoPedido;
+    public Ingrediente IngredienteProibidoPedido => ingredienteProibidoPedido;
 }
