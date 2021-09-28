f72b86d8322d4e30556abaf23a9180d0a212bb42
Eduarda Paolly <paollyk13@outlook.com>
2021-09-28 18:21:47 -0300

    ListaDeReceitas.script Updated
    We have created a vector of Receitas where we can put the number os recipes in the game and make a list from them, and serialized

--- ProjectCookingShark/Assets/Script/ListaDeReceitas.cs
+++ ProjectCookingShark/Assets/Script/ListaDeReceitas.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ListaDeReceitas : Receita
+public class ListaDeReceitas : MonoBehaviour
 {
-   
+    [SerializeField] Receita[] receitaPrato;
+    public Receita[] ReceitaPrato => receitaPrato;
+
+    //List<Receita> receita = new List<Receita>();
+    
 }
