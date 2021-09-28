1ddb2bc1a732bae907a266013748e25459175146
Eduarda Paolly <paollyk13@outlook.com>
2021-09-28 17:43:36 -0300

    Created Pedido.script
    The script Pedido has been created

--- ProjectCookingShark/Assets/Script/Pedido.cs
+++ ProjectCookingShark/Assets/Script/Pedido.cs
@@ -5,12 +5,4 @@ using UnityEngine;
 public class Pedido : MonoBehaviour
 {
     
-    //Dictionary<string, Ingrediente> sabor = new Dictionary<string, Ingrediente>();
-
-    [SerializeField] Sabores.SaboresExistentes sabor01;
-    [SerializeField] [Range(0, 10)] int intensidadeSabor01;
-    [SerializeField] Sabores.SaboresExistentes sabor02;
-    [SerializeField] [Range(0, 10)] int intensidadeSabor02;
-    [SerializeField] Ingrediente ingredienteProibido;
-
 }
