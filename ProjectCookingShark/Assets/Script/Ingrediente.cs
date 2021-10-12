ff81bbd0d18f9f9d3d37647f5df12d738468584d
GeovaneLJunior <44369720+GeovaneLJunior@users.noreply.github.com>
2021-10-12 18:08:34 -0400

    Drag and mouseUp
    Drag and MouseUP funciontions has been implemented

--- ProjectCookingShark/Assets/Script/Ingrediente.cs
+++ ProjectCookingShark/Assets/Script/Ingrediente.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class Ingrediente : Sabores
 {
-
     [SerializeField] string nome;
     public string Nome => nome;
     [SerializeField] Sabores.SaboresExistentes sabor;
     public Sabores.SaboresExistentes Sabor => sabor;
+
+    private Vector2 _offset, _originalPosition;
+
+    private bool _dragging;
+
+    private void Awake()
+    {
+        _originalPosition = transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMousePos();
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = _originalPosition;
+    }
+
+    Vector3 GetMousePos()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+
 
 }
